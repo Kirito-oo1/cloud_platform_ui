e8ff4bc31e747fe33e70166e4829a4c9f9d93346
boboobee <2861851422@qq.com>
2024-12-17 17:16:31 +0800

    修改高斯页面

--- src/views/HomeView/main/systemFunction/gaussianModel/index.vue
+++ src/views/HomeView/main/systemFunction/gaussianModel/index.vue
@@ -2,7 +2,7 @@
 <template>
     <div style="width:100%; height:100%;">
       <iframe 
-        src="https://playcanvas.com/supersplat/editor"
+        src="http://localhost:3000"
         frameborder="0" 
         style="width:100%; height:100%;"
       >
@@ -16,7 +16,7 @@
     data:{
      //src:"http://localhost:3000" 
      
-     src:"https://playcanvas.com/supersplat/editor" 
+     //src:"https://playcanvas.com/supersplat/editor" 
     }
   };
   </script>
